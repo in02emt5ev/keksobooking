02022f65338ae7e97a1e0df23d0a6f73dfbbcb16
in02emt5ev <in02emt5ev@vk.com>
2026-05-02 01:50:45 +0700

    Добавляет функцию генерирования случайных чисел с плавающей точкой в заданном диапазоне с определенным колличеством знаков после запятой

--- js/main.js
+++ js/main.js
@@ -10,4 +10,19 @@ const getRandomPositiveInt = (a, b = 1) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-console.log(getRandomPositiveInt(0, 10));
+const getRandomPositiveFloat = (a, b = 0, decim = 0) => {
+
+  if (a === undefined) {
+    throw new Error("Некорректный диапазон")
+  }
+
+  const min = Math.min(Math.abs(a), Math.abs(b));
+  const max = Math.max(Math.abs(a), Math.abs(b));
+
+  const result = (Math.random() * (max - min)) + min;
+
+  return Number(result.toFixed(decim));
+}
+
+getRandomPositiveInt(0, 1);
+getRandomPositiveFloat(0.78, 1.33, 3)
