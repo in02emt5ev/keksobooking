6cd6516993fefe040c71100e2e1de596c09a68a4
in02emt5ev <in02emt5ev@vk.com>
2026-05-29 20:07:16 +0700

    Добавляем функцию создания карточки объявления

--- js/dom.js
+++ js/dom.js
@@ -1,1 +1,9 @@
 const adCardTemplate = document.querySelector('#card').content.querySelector('.popup');
+
+const createAdCard = (cardData) => {
+  const card = adCardTemplate.cloneNode(true);
+
+  console.log(card);
+};
+
+createAdCard();
